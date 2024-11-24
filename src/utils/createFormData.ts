3bb973c231b88f3083b5f4743f12bbdbb3ca40b4
Yonatan Ariel <yonatanriel@gmail.com>
2024-11-24 23:48:58 +0200

    deleting comment

--- src/utils/createFormData.ts
+++ src/utils/createFormData.ts
@@ -10,19 +10,3 @@ export const createFormData = (data: SignUpFormValues): FormData => {
   }
   return formData;
 };
-// for (const name in data) {
-//   if (!Object.prototype.hasOwnProperty.call(data, name)) continue;
-
-//   const value = data[name as keyof SignUpFormValues];
-
-//   if (Array.isArray(value)) {
-//     formData.append(name, JSON.stringify(value));
-//   } else {
-//     formData.append(name, value as string);
-//   }
-// }
-
-// if (data?.photo instanceof File) {
-//   formData.append("photo", data.photo);
-// }
-// console.log(formData.get("userName"));
